185cc77a7d98eaf67a17fe3faa56b4ae717092e6
danu3268 <razvan.ionica@avaelgo.ro>
2014-10-18 18:06:05 +0300

    UI implementatrion

---
+++ Feromon.Web/Scripts/googleAPIs/GoogleLocationAutocomplete.js
@@ -0,0 +1,38 @@
+﻿function initialize() {
+    var input = document.getElementById('pac-input');
+    var types = document.getElementById('type-selector');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    var infowindow = new google.maps.InfoWindow();
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        infowindow.close();
+        var place = autocomplete.getPlace();
+        $("#geopositionX").val(place.geometry.location.A);
+        $("#geopositionY").val(place.geometry.location.k);
+        if (!place.geometry) {
+            return;
+        }
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    });
+
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    function setupClickListener(id, types) {
+        var radioButton = document.getElementById(id);
+        google.maps.event.addDomListener(radioButton, 'click', function() {
+            autocomplete.setTypes(types);
+        });
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
