187f25d74ce77c18236b150297afef57ef7d64f7
danu3268 <razvan.ionica@avaelgo.ro>
2014-10-19 14:17:09 +0300

    view friends

---
+++ Feromon.Web/Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feromon.Web.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpPost]
+        public ActionResult ShootSelectedPersons(List<string> selectedPersonList)
+        {
+            foreach (var person in selectedPersonList)
+            {
+                //save in database
+            }
+            return View();
+        }
+    }
+}
