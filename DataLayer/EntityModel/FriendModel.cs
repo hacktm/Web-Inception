dffcd3274f9b12420e1fefb7a6689658b8f6be14
fdemain <adi@arduino.ro>
2014-10-19 10:25:39 +0300

    j

---
+++ DataLayer/EntityModel/FriendModel.cs
@@ -0,0 +1,21 @@
+﻿namespace DataLayer.EntityModel
+{
+    public class FriendModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string category { get; set; }
+
+        public enum list_type
+        {
+            close_friends,
+            acquaintances,
+            restricted,
+            user_created,
+            education,
+            work,
+            current_city,
+            family
+        }
+    }
+}
---
+++ DataLayer/EntityModel/InviteFriendModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.EntityModel
+{
+    public class InviteFriendModel
+    {
+        public string InviteToken { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
